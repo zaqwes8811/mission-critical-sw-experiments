045660d396773530b9414e25ec7500e6a8c3ea11
Igor Lugansky <ilugansky@arrival.com>
2022-09-08 10:59:25 +0300

    heap disable

--- safe/safe_eval.cpp
+++ safe/safe_eval.cpp
@@ -4,13 +4,17 @@
 
 //#include <typeinfo>
 
+#include <stdlib.h>
 
-[[nodiscard]]
-void* rtti(   ) noexcept {
-//    auto a =typeid(int);
-//throw int();
+[[nodiscard]] void* rtti() noexcept {
+    //    auto a =typeid(int);
+    // throw int();
+    int* ptr = new int();
+    (void)ptr;
+
+    void* p = malloc(10);
+    (void)p;
+    return nullptr;
 }
 
-void g() {
-    rtti();
-}
+void g() { (void)rtti(); }
---
+++ safe/test_nomalloc.cpp
@@ -0,0 +1,24 @@
+//
+// Created by zaqwes on 08.09.2022.
+//
+
+#include <stdlib.h>
+
+// "Reducing Memory Allocations in a Large C++ Application - Arnaud Desitter [ C++ on Sea 2020 ]"
+// https://www.youtube.com/watch?v=ZXTI5iWHhrg&ab_channel=cpponsea
+
+// NoHeap class
+// https://en.wikibooks.org/wiki/More_C%2B%2B_Idioms/Requiring_or_Prohibiting_Heap-based_Objects
+
+// Embedded
+// https://www.embedded.com/preventing-dynamic-allocation/
+
+// https://stackoverflow.com/questions/262439/create-a-wrapper-function-for-malloc-and-free-in-c
+
+int main() {
+//#ifdef USE_MALLOC
+    auto p = malloc(10);
+    (void)p;
+//#endif
+    return 0;
+}
