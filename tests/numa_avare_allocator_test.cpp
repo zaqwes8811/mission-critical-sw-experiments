3ff7ab06fc31c3a19c4df68bb3ee35015d7a9852
Igor Lugansky <ilugansky@arrival.com>
2022-09-13 18:02:24 +0300

    ave

--- tests/numa_avare_allocator_test.cpp
+++ tests/numa_avare_allocator_test.cpp
@@ -7,3 +7,14 @@
 
 // example
 // https://github.com/ReidAtcheson/numaallocator/blob/master/numaalloc.h
+
+// Best practice?
+// https://scicomp.stackexchange.com/questions/2028/portable-multicore-numa-memory-allocation-initialization-best-practices
+
+// https://stackoverflow.com/questions/7986903/can-i-get-the-numa-node-from-a-pointer-address-in-c-on-linux
+
+// Heap - ok? but stack? local vars?
+// https://linux.die.net/man/2/get_mempolicy
+
+// boost
+// https://www.boost.org/doc/libs/1_66_0/libs/fiber/doc/html/fiber/numa.html
