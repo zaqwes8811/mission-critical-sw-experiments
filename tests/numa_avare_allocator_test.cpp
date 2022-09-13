aef4524367c9724244b072e8202a3ca953ae2aac
Igor Lugansky <ilugansky@arrival.com>
2022-09-13 16:22:00 +0300

    save

---
+++ tests/numa_avare_allocator_test.cpp
@@ -0,0 +1,6 @@
+//
+// Created by zaqwes on 13.09.2022.
+//
+
+// https://stackoverflow.com/questions/11126093/how-do-i-know-if-my-server-has-numa
+// https://stackoverflow.com/questions/8154162/numa-aware-cache-aligned-memory-allocation
