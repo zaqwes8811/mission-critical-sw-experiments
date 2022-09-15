f1f7b25cd48efcaca042b6bfb11d416392939701
Igor Lugansky <ilugansky@arrival.com>
2022-09-15 18:11:47 +0300

    reorder

--- tests/tight_alloc_test.cpp
+++ tests/tight_alloc_test.cpp
@@ -2,81 +2,79 @@
 // Created by zaqwes on 14.09.2022.
 //
 
+// Vector strings of known max size in single peace of memory
+
+// Arena
+//   https://www.gingerbill.org/article/2019/02/08/memory-allocation-strategies-002/
+// std::pmr
+// vector control / set? | str-head-1/str-body-1 | str-head-2/str-body-2
+// https://www.gingerbill.org/article/2019/02/08/memory-allocation-strategies-002/
+
 #include <gtest/gtest.h>
 
 #include <iomanip>
 #include <unordered_set>
 
-// Vector strings of known max size in single peace of memory
-
-// Arena
-//   https://www.gingerbill.org/article/2019/02/08/memory-allocation-strategies-002/
-
-// std::pmr
-
-// vector control / set? | str-head-1/str-body-1 | str-head-2/str-body-2
-
-// https://www.gingerbill.org/article/2019/02/08/memory-allocation-strategies-002/
-auto &o = std::cout;
-// auto& nl =  std::endl;
-
 namespace arena {
-constexpr bool is_power_of_two(uintptr_t x) { return (x & (x - 1)) == 0; }
 
-constexpr uintptr_t align_forward(uintptr_t ptr, size_t align) {
-    assert(is_power_of_two(align));
-
-    uintptr_t p = ptr;
-    const auto a = static_cast<uintptr_t>(align);
-
-    // Same as (p % a) but faster as 'a' is a power of two
-    const uintptr_t modulo = p & (a - 1);
-
-    if (modulo != 0) {
-        // If 'p' address is not aligned, push the address to the
-        // next value which is aligned
-        p += a - modulo;
+class Arena {
+public:
+    explicit Arena(size_t buf_len) : underlying_storage_(buf_len, 0xae), buf_len_{buf_len} {
+        buf = underlying_storage_.data();
     }
-    return p;
-}
 
-struct Arena {
-    Arena(unsigned char *buf_, size_t buf_len_) : buf{buf_}, buf_len{buf_len_} {
-        o << "Arena: start_addr: " << std::hex << static_cast<void *>(buf_) << std::dec << std::endl;
+    // Idea: Max alignment is pessimistic
+    [[nodiscard]] void *alloc(size_t size, size_t align = alignof(std::max_align_t)) {
+        auto a = this;
+        // Align 'curr_offset' forward to the specified alignment
+        const uintptr_t curr_ptr = (uintptr_t)a->buf + (uintptr_t)a->curr_offset;
+        const uintptr_t offset = align_forward(curr_ptr, align) - (uintptr_t)a->buf;  // Change to relative offset
+
+        // Check to see if the backing memory has space left
+        if (offset + size <= a->buf_len_) {
+            a->curr_offset = offset + size;
+
+            // Zero new memory by default
+            void *ptr = &a->buf[offset];
+            memset(ptr, 0, size);
+
+            return ptr;
+        }
+        // Return NULL if the arena is out of memory (or handle differently)
+        return nullptr;
     }
+
+    [[nodiscard]] const auto &storage() const { return underlying_storage_; }
+
+private:
+    [[nodiscard]] static constexpr bool is_power_of_two(uintptr_t x) { return (x & (x - 1)) == 0; }
+
+    [[nodiscard]] static constexpr uintptr_t align_forward(uintptr_t ptr, size_t align) {
+        assert(is_power_of_two(align));
+
+        uintptr_t p = ptr;
+        const auto a = static_cast<uintptr_t>(align);
+
+        // Same as (p % a) but faster as 'a' is a power of two
+        const uintptr_t modulo = p & (a - 1);
+
+        if (modulo != 0) {
+            // If 'p' address is not aligned, push the address to the
+            // next value which is aligned
+            p += a - modulo;
+        }
+        return p;
+    }
+
     unsigned char *buf{};
-    size_t buf_len{};
+    const size_t buf_len_{};
     size_t curr_offset{0};
+    std::vector<unsigned char> underlying_storage_;
 };
 
-void *arena_alloc_align(Arena *a, size_t size, size_t align) {
-    o << "Arena: size:" << size << " align:" << align << std::endl;
-    // Align 'curr_offset' forward to the specified alignment
-    const uintptr_t curr_ptr = (uintptr_t)a->buf + (uintptr_t)a->curr_offset;
-    const uintptr_t offset = align_forward(curr_ptr, align) - (uintptr_t)a->buf;  // Change to relative offset
-
-    // Check to see if the backing memory has space left
-    if (offset + size <= a->buf_len) {
-        a->curr_offset = offset + size;
-
-        // Zero new memory by default
-        void *ptr = &a->buf[offset];
-        memset(ptr, 0, size);
-
-        o << "Arena: alloc_addr: " << std::hex << static_cast<void *>(ptr) << std::dec << std::endl;
-        return ptr;
-    }
-    // Return NULL if the arena is out of memory (or handle differently)
-    return nullptr;
-}
-
-// Because C doesn't have default parameters
-// Idea: Max alignment is pessimistic
-void *arena_alloc(Arena *a, size_t size) { return arena_alloc_align(a, size, alignof(std::max_align_t)); }
-
-// https://stackoverflow.com/questions/48061522/create-the-simplest-allocator-with-two-template-arguments
 template <class T>
 class ArenaAllocator {
+    // https://stackoverflow.com/questions/48061522/create-the-simplest-allocator-with-two-template-arguments
 public:
     typedef T value_type;
     typedef value_type *pointer;
@@ -85,8 +83,6 @@ public:
     typedef const value_type &const_reference;
     typedef std::size_t size_type;
     typedef std::ptrdiff_t difference_type;
-
-    //    using value_type = T;
     using propagate_on_container_move_assignment = std::true_type;
     using is_always_equal = std::true_type;
 
@@ -102,10 +98,9 @@ public:
     }
 
     inline pointer allocate(size_t num) {
-        auto ptr = arena_alloc(a_, num * sizeof(T));
-        //        auto ptr = malloc(num * sizeof(T));
+        auto ptr = a_->alloc(num * sizeof(T), alignof(T));
         if (!ptr) throw std::bad_alloc();
-        return reinterpret_cast<T *>(ptr);
+        return reinterpret_cast<pointer>(ptr);
     }
 
     void deallocate(pointer p, size_t num) noexcept {}
@@ -115,7 +110,6 @@ public:
         typedef ArenaAllocator<U> other;
     };
 
-    // private:
     Arena *a_ = nullptr;
 };
 
@@ -130,7 +124,13 @@ bool operator!=(const ArenaAllocator<T1> &, const ArenaAllocator<T2> &) noexcept
 }
 }  // namespace arena
 
+//========================================================================================
+//========================================================================================
+//========================================================================================
+
 namespace {
+auto &o = std::cout;
+
 void dump(const std::vector<unsigned char> &arena_buffer) {
     int ptr = 0;
 
@@ -165,9 +165,8 @@ using String = std::basic_string<char, std::char_traits<char>, arena::ArenaAlloc
 
 TEST(TightAllocTest, ArenaUnderlyingApi) {
     using namespace arena;
-    auto arena_buffer = std::vector<unsigned char>(256, 0);
-    auto arena = Arena{arena_buffer.data(), arena_buffer.size()};
-    dump(arena_buffer);
+    auto arena = Arena{256 + 64};
+    dump(arena.storage());
 
     auto allocator = ArenaAllocator<int>{&arena};
     //    {
@@ -178,24 +177,24 @@ TEST(TightAllocTest, ArenaUnderlyingApi) {
 
     {
         using A = ArenaVector<int>;
-        //        auto a = (A *)arena_alloc(&arena, sizeof(A));
-        //        new (a) A(allocator);
-        //        a->reserve(5);
-        //        a->push_back(9);
+        auto a = (A *)arena.alloc(sizeof(A), alignof(A));
+        new (a) A(allocator);
+        a->reserve(5);
+        a->push_back(9);
     }
-    dump(arena_buffer);
+    dump(arena.storage());
 
     {
-        // TODO() Need rebind for allocator or something
         //        using A = std::unordered_set<int, std::hash<int>, std::equal_to<>, ArenaAllocator<int>>;
-        using A = std::set<int, std::less<>, ArenaAllocator<int>>;  // Need extra work
-        auto a = (A *)arena_alloc(&arena, sizeof(A));
+        using A = std::set<int, std::less<>, ArenaAllocator<int>>;
+        auto a = (A *)arena.alloc(sizeof(A), alignof(A));
         ASSERT_NE(a, nullptr);
         new (a) A(allocator);
         a->insert(9);
         a->insert(8);
+        a->~A();
     }
-    dump(arena_buffer);
+    dump(arena.storage());
 
     // String
     // https://stackoverflow.com/questions/37502974/stdstring-with-a-custom-allocator
@@ -203,7 +202,7 @@ TEST(TightAllocTest, ArenaUnderlyingApi) {
     //        auto allocator = ArenaAllocator<char>{&arena};
     //
     //        using A = String;
-    //        auto a = (A *)arena_alloc(&arena, sizeof(A));
+    //        auto a = (A *)alloc(&arena, sizeof(A));
     //        ASSERT_NE(a, nullptr);
     //        dump(arena_buffer);
     //        new (a) A(allocator);
@@ -211,5 +210,5 @@ TEST(TightAllocTest, ArenaUnderlyingApi) {
     //        *a += "1234";
     //        //        *a += "1234567812345678";
     //    }
-    dump(arena_buffer);
+    dump(arena.storage());
 }
