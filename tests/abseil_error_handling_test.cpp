4782c4a65c20782727b62ead32f06899f63806ea
Igor Lugansky <ilugansky@arrival.com>
2022-11-17 18:18:05 +0400

    save

---
+++ tests/abseil_error_handling_test.cpp
@@ -0,0 +1,11 @@
+
+#include <absl/status/status.h>
+#include <absl/status/statusor.h>
+
+#include <gtest/gtest.h>
+
+TEST(AbseilStatusTest, StealValue) {
+    absl::StatusOr<int> maybe_url;// = lex.ParseUtf8();
+//    RETURN_IF_ERROR(maybe_url.status());
+//    type_url = std::move(maybe_url)->value;
+}
